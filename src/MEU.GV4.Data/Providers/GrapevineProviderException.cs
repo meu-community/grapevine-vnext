1e6d0ac6c9018df43f863e3ba6ecdaa2a9f97f48
Tory Alsip <13713268+toryalsip@users.noreply.github.com>
2025-03-23 16:22:17 -0400

    Add GrapevineProviderException to throw when validation fails

---
+++ src/MEU.GV4.Data/Providers/GrapevineProviderException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEU.GV4.Data.Providers
+{
+    public class GrapevineProviderException : Exception
+    {
+        public GrapevineProviderException(string message) : base(message) { }
+        public GrapevineProviderException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
