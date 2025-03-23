1b29c1beb9c2dc49d0803c534bcd1fd7dea70f59
Tory Alsip <13713268+toryalsip@users.noreply.github.com>
2025-03-23 18:06:53 -0400

    Add XmlHelper and refactor existing code (#4)

---
+++ src/MEU.GV4.Data/Helpers/XmlHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml;
+
+namespace MEU.GV4.Data.Helpers
+{
+    public class XmlHelper
+    {
+        public static string? GetAttribute(XmlElement? element, string attributeName)
+        {
+            return element?.Attributes?.GetNamedItem(attributeName)?.Value;
+        }
+
+        public static string? GetCData(XmlElement? element, string elementName)
+        {
+            return element?.SelectSingleNode(elementName)?.FirstChild?.Value;
+        }
+    }
+}
---
+++ tests/MEU.GV4.Data.Tests/Helpers/XmlHelperTests.cs
@@ -0,0 +1,35 @@
+﻿using MEU.GV4.Data.Helpers;
+using System.Xml;
+
+namespace MEU.GV4.Data.Tests.Helpers
+{
+    public class XmlHelperTests
+    {
+        [Fact(DisplayName = "Can read attribute from an element")]
+        public void CanReadAttributeFromElement()
+        {
+            var testDoc = new XmlDocument();
+            testDoc.LoadXml("""
+                <?xml version="1.0"?>
+                <test attr1="foo">
+                </test>
+                """);
+            Assert.Equal("foo", XmlHelper.GetAttribute(testDoc.DocumentElement, "attr1"));
+        }
+
+        [Fact(DisplayName = "Can read CData value from child element")]
+        public void CanReadCDataChildElement()
+        {
+            var testDoc = new XmlDocument();
+            testDoc.LoadXml("""
+                <?xml version="1.0"?>
+                <test>
+                    <foo>
+                      <![CDATA[bar]]>
+                    </foo>
+                </test>
+                """);
+            Assert.Equal("bar", XmlHelper.GetCData(testDoc.DocumentElement, "foo"));
+        }
+    }
+}
