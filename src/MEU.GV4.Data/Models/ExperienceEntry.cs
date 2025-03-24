d90c9853109334314652caba67a1a8c612c56582
Tory Alsip <13713268+toryalsip@users.noreply.github.com>
2025-03-23 20:51:24 -0400

    remove unused usings from ExperienceEntry

--- src/MEU.GV4.Data/Models/ExperienceEntry.cs
+++ src/MEU.GV4.Data/Models/ExperienceEntry.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MEU.GV4.Data.Models
+﻿namespace MEU.GV4.Data.Models
 {
     public enum ExperienceChangeType
     {
