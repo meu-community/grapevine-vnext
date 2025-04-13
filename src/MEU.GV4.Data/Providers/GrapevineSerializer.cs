089ea4b4c0e28b5688f9f7657c1adb7cda794596
Tory Alsip <13713268+toryalsip@users.noreply.github.com>
2025-04-12 20:51:27 -0500

    Create Game Data Serializer (#44)

---
+++ src/MEU.GV4.Data/Providers/GrapevineSerializer.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+using MEU.GV4.Data.Models;
+
+namespace MEU.GV4.Data.Providers;
+public class GrapevineSerializer : IGameSerializer
+{
+    private JsonSerializerOptions options = new JsonSerializerOptions
+    {
+        WriteIndented = true
+    };
+
+    public GrapevineSerializer(IJsonTypeInfoResolver typeInfoResolver)
+    {
+        options.TypeInfoResolver = typeInfoResolver;
+    }
+
+
+    public async Task<Game?> DeserializeAsync(Stream input)
+    {
+        if (input == null || input.Length == 0)
+        {
+            throw new GrapevineProviderException(ErrorConstants.FILE_CONTENTS_EMPTY);
+        }
+        try
+        {
+            return await JsonSerializer.DeserializeAsync<Game?>(input, options);
+        }
+        catch (JsonException ex)
+        {
+            throw new GrapevineProviderException(ErrorConstants.INVALID_GRAPEVINE_FILE, ex);
+        }
+    }
+
+    public async Task SerializeAsync(Stream output, Game game)
+    {
+        await JsonSerializer.SerializeAsync(output, game, options);
+    }
+}
